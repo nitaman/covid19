2c952a3c0f138777a1aed2f2e411879a39117247
github <s_nita@daily-tohoku.co.jp>
2021-09-24 15:01:42 +0900

    [Add]cron setting

--- get_file.php
+++ get_file.php
@@ -7,7 +7,7 @@ $arr = json_decode($json,true);
 
 //$path = '/var/www/html/wp-content/themes/understrap-child-dtnews/covid/';
 //$path = '/Users/dailytohoku/AWS/react/covid/public/data/';
-$path = '/home/runner/work/covid19/covid19/';
+$path = './';
 
 $target = $path.'*.csv';
 $result = glob($target);
@@ -19,6 +19,7 @@ if(empty($result)) {
 }
 */
 $localFilePath = $path;
+
 for($i=0; $i<count($arr['result']['resources']); $i++) {
   $csvName = $arr['result']['resources'][$i]['name'];
   $csvUpDate = $arr['result']['resources'][$i]['updated'];

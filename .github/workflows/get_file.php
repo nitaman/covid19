67452a3dc80e53cd35a6e7169b7e4e97ec74af69
github <s_nita@daily-tohoku.co.jp>
2021-09-24 17:20:23 +0900

    [Add]cron setting

--- .github/workflows/get_file.php
+++ .github/workflows/get_file.php
@@ -5,28 +5,5 @@ $json = file_get_contents($filename);
 $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
 $arr = json_decode($json,true);
 
-//$path = '/var/www/html/wp-content/themes/understrap-child-dtnews/covid/';
-//$path = '/Users/dailytohoku/AWS/react/covid/public/data/';
-$path = '/home/runner/work/covid19/covid19/';
-
-$target = $path.'*.csv';
-$result = glob($target);
-/*
-if(empty($result)) {
-  $localFilePath = '';
-}else{
-  $localFilePath = $path;
-}
-*/
-$localFilePath = $path;
-
-for($i=0; $i<count($arr['result']['resources']); $i++) {
-  $csvName = $arr['result']['resources'][$i]['name'];
-  $csvUpDate = $arr['result']['resources'][$i]['updated'];
-  $csvOrder = explode('_', $csvName)[0];
-  $csvData = file_get_contents($arr['result']['resources'][$i]['download_url']);
-  $csvData = mb_convert_encoding($csvData, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-  $file = $path.$csvOrder.'.csv';
-  file_put_contents($file, $csvData); // 処理の結果をファイルに書き出す
 }
 ?>

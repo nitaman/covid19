943a9dc48eeb4a8c87b4e8825828a8c7e718709a
github <s_nita@daily-tohoku.co.jp>
2021-09-24 17:26:34 +0900

    [Add]cron setting

--- .github/workflows/get_file.php
+++
@@ -1,9 +0,0 @@
-<?php
-// オープンデータの配信ファイルリストjsonから各データのURLを取得
-$filename = 'https://opendata.pref.aomori.lg.jp/api/package_show?id=5e4612ce-1636-41d9-82a3-c5130a79ffe0';
-$json = file_get_contents($filename);
-$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-$arr = json_decode($json,true);
-
-}
-?>
